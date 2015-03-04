3ddeabaefa1fdea9b37688101142244666274ee6
Raphael Hüni <rafhungmail.com>
2015-03-04 06:30:52 +0100

    configure sass tasks > > The sass tasks are now setup to provide the banner needed by Wordpress for the theme > information. This is added to the stylesheet in the main sass task. The output of the > autoprefixer task then is directly put into the dest folder as it will be used as the > reference for the theme info as well as for the styleguide stylesheet. > The minifier then runs on that file generating the style.min.css that is used throughout > the theme.

--- grunt/sass.js
+++ grunt/sass.js
@@ -3,7 +3,20 @@ module.exports = {
     options: {
       style: 'expanded',
       bundleExec: true,
-      require: 'susy'
+      require: 'susy',
+      banner: '/*\n' +
+              ' * Theme Name: <%= pkg.themeName %>\n' +
+              ' * Theme URI: \n' +
+              ' * Author: <%= pkg.author %>\n' +
+              ' * Author URI: \n' +
+              ' * Description: Theme developed by rafhun from Werbelinie AG for ...\n' +
+              ' * Version: <%= pkg.version %>\n' +
+              ' * License: <%= pkg.license %>\n' +
+              ' * License URI: http://www.gnu.org/licenses/gpl-2.0.html\n' +
+              ' * Tags: \n' +
+              ' * Text Domain: <%= pkg.themeName %>\n\n' +
+              ' * This stylesheet is not used by this Wordpress site it only exists as reference for Wordpress. The stylesheet in use can be found in this folder as style.min.{hash}.css\n' +
+              ' */'
     },
     src: '<%= pkg.srcFolders.scss %>main.scss',
     dest: '<%= pkg.srcFolders.css %>style.css',
