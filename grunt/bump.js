4655ac43862e84deb37d46a2e696f3e7c5d8ed70
Raphael Hüni <rafhungmail.com>
2015-06-04 17:39:58 +0200

    fix(build): fix bump config
    a mistake in the bump config led to it only using
    the standard options not the ones provided in the
    file. With this fix correct results especially from
    the push can be expected

--- grunt/bump.js
+++ grunt/bump.js
@@ -1,20 +1,18 @@
 module.exports = {
-  bump: {
-    options: {
-      files: ['package.json', 'bower.json'],
-      updateConfigs: ['pkg'],
-      commit: true,
-      commitMessage: 'Release v%VERSION%',
-      commitFiles: ['package.json', 'bower.json'],
-      createTag: true,
-      tagName: 'v%VERSION%',
-      tagMessage: 'Version %VERSION%',
-      push: true,
-      pushTo: 'upstream',
-      gitDescribeOptions: '--tags --always --abbrev=1 --dirty=-d',
-      globalReplace: false,
-      prereleaseName: false,
-      regExp: false
-    }
-  },
+  options: {
+    files: ['package.json', 'bower.json', 'readme.md'],
+    updateConfigs: ['pkg'],
+    commit: true,
+    commitMessage: 'Release v%VERSION%',
+    commitFiles: ['package.json', 'bower.json', 'readme.md'],
+    createTag: true,
+    tagName: 'v%VERSION%',
+    tagMessage: 'Version %VERSION%',
+    push: true,
+    pushTo: 'origin',
+    gitDescribeOptions: '--tags --always --abbrev=1 --dirty=-d',
+    globalReplace: false,
+    prereleaseName: false,
+    regExp: false
+  }
 }
