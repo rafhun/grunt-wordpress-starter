eddcdace877ba251ed61bde7a63d327797ad3936
Raphael Hüni <rafhungmail.com>
2015-08-20 13:05:19 +0200

    fix(grunt): reference correct config file for getWP

--- grunt/shell.js
+++ grunt/shell.js
@@ -9,7 +9,7 @@ module.exports = {
     command: 'git add CHANGELOG.md && git commit --amend --no-edit && git tag v<%= pkg.version %> -m "Version <%= pkg.version %>" && git push && git push --tags'
   },
   getWP: {
-    command: 'wget https://wordpress.org/latest.tar.gz && tar zxvf latest.tar.gz && mv wordpress <%= pkg.rootFolder %> && rm latest.tar.gz'
+    command: 'wget https://wordpress.org/latest.tar.gz && tar zxvf latest.tar.gz && mv wordpress <%= config.rootFolder %> && rm latest.tar.gz'
   },
   getKeys: {
     command: 'curl https://api.wordpress.org/secret-key/1.1/salt/ > <%= config.srcFolders.secrets %>keys.php'
