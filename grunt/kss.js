9ef4b466ce2b38d26562ec2517c21a8739db175a
Raphael Hüni <hueni@werbelinie.ch>
2015-09-22 13:45:01 +0200

    fix(grunt): correctly import kss assets

--- grunt/kss.js
+++ grunt/kss.js
@@ -1,12 +1,12 @@
 module.exports = {
   options: {
-    css: ['<%= destFolder %>style.css', '<%= destFolder %><%= config.buildFolders.icons %>icons.data.svg.css'],
-    js: '<%= destFolder %>script.js',
-    template: 'node_modules/grunt-kss/node_modules/kss/generator/handlebars/template/'
+    css: ['/<%= config.buildFolders.theme %><%= config.themeName %>/style.css', '/<%= config.buildFolders.theme %><%= config.themeName %>/<%= config.buildFolders.icons %>icons.data.svg.css'],
+    js: '/<%= config.buildFolders.theme %><%= config.themeName %>/script.js',
+    // template: 'node_modules/grunt-kss/node_modules/kss/generator/handlebars/template/'
   },
   styleguide: {
     files: {
-      '<%= config.buildFolders.styleguide %>' : ['<%= config.srcFolders.scss %>']
+      '<%= config.rootFolder %><%= config.buildFolders.styleguide %>' : ['<%= config.srcFolders.scss %>']
     }
   }
 }
