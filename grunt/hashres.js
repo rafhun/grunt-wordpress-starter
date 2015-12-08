9c8927cb1f74d23a15d4ef387bbf09eab1a9fb2e
Raphael Hüni <hueni@werbelinie.ch>
2015-12-08 16:36:35 +0100

    fix(hashres): use full stylesheet names instead of * to prevent hashes added to file name
    Using the universal * selector leads to the hash being added to the icon stylesheet
    again and again leading to too long filenames over time. This can only be fixed by
    using the full filenames in the hashres config instead of a universal selector.

--- grunt/hashres.js
+++ grunt/hashres.js
@@ -7,8 +7,10 @@ module.exports = {
     src: [
       '<%= destFolder %>style.min.css',
       '<%= destFolder %><%= config.buildFolders.js %>script.min.js',
-      '<%= destFolder %><%= config.buildFolders.icons %>*.css'
+      '<%= destFolder %><%= config.buildFolders.icons %>icons.data.svg.css',
+      '<%= destFolder %><%= config.buildFolders.icons %>icons.data.png.css',
+      '<%= destFolder %><%= config.buildFolders.icons %>icons.fallback.css'
     ],
-    dest: ['<%=destFolder %>functions.php', '<%= destFolder %>header.php', '<%= config.srcFolders.config %>kss/template/index.html']
+    dest: ['<%=destFolder %>functions.php', '<%= destFolder %>header.php', '<%= config.srcFolders.config %>kss-dest/template/index.html']
   }
 }
