cfae792bddc1ca715480d2e848c215824afffaed
Raphael Hüni <hueni@werbelinie.ch>
2016-03-07 15:29:18 +0100

    feat(grunt): better sass compilation through options
    Enable sourcemaps and include bower_components and node_modules paths
    for imports to ease @import definitions in our sass.

--- grunt/sass.js
+++ grunt/sass.js
@@ -1,4 +1,8 @@
 module.exports = {
+  options: {
+    sourceMap: true,
+    includePaths: ['bower_components/', 'node_modules/']
+  },
   main: {
     src: '<%= config.srcFolders.scss %>style.scss',
     dest: '<%= config.srcFolders.css %>style.css',
