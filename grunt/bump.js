b0132e1666ecd42abe233d693f0daf2352f970a3
Raphael Hüni <hueni@werbelinie.ch>
2016-03-07 16:09:58 +0100

    style(grunt): remove default bump settings from config

--- grunt/bump.js
+++ grunt/bump.js
@@ -3,16 +3,8 @@ module.exports = {
     files: ['package.json', 'bower.json', 'readme.md'],
     updateConfigs: ['pkg'],
     commit: true,
-    commitMessage: 'Release v%VERSION%',
     commitFiles: ['package.json', 'bower.json', 'readme.md'],
     createTag: false,
-    tagName: 'v%VERSION%',
-    tagMessage: 'Version %VERSION%',
-    push: false,
-    pushTo: 'origin',
-    gitDescribeOptions: '--tags --always --abbrev=1 --dirty=-d',
-    globalReplace: false,
-    prereleaseName: false,
-    regExp: false
+    push: false
   }
 }
