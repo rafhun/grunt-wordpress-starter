db8be8beb253dc175ca3d0665049568d145ce376
Raphael Hüni <hueni@werbelinie.ch>
2016-03-07 15:15:33 +0100

    feat(grunt): optimize uglify with options
    Add sourcemaps, removes all comments and screws IE8 and below
    since these browsers are no longer a requirement here.

--- grunt/uglify.js
+++ grunt/uglify.js
@@ -1,12 +1,13 @@
 module.exports = {
+  options: {
+    sourceMap: true,
+    sourceMapIncludeSources: true,
+    sourceMapIn: '<%= concat.dist.dest %>.map',
+    preserveComments: false,
+    screwIE8: true
+  },
   build: {
     src: '<%= concat.dist.dest %>',
     dest: '<%= destFolder %><%= config.buildFolders.js %>script.min.js'
-  },
-  nonconcat: {
-    expand: true,
-    cwd: '<%= config.srcFolders.js %>',
-    src: ['*.js', '!{bower,script}.js'],
-    dest: '<%= destFolder %><%= config.buildFolders.js %>'
   }
 }
