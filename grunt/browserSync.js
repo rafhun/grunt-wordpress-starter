fd66f8e6ddac188bb70186751ad4a7df507d14c8
Raphael Hüni <hueni@werbelinie.ch>
2016-03-07 16:05:58 +0100

    feat(grunt): optimize browserSync configuration
    We remove the notifications, set a default reloadDelay of 1sec and add
    some commented out options that might come in handy during development.

--- grunt/browserSync.js
+++ grunt/browserSync.js
@@ -6,7 +6,16 @@ module.exports = {
     options: {
       watchTask: true,
       proxy: '<%= config.devUrl %>',
-      startPath: '<%= config.buildFolders.styleguide %>'
+      startPath: '<%= config.buildFolders.styleguide %>',
+      // enable if you encounter any problems with browserSync
+      // logLevel: 'debug',
+      notify: false,
+      // since we often have copying and rehashing going on, this makes sense
+      reloadDelay: 1000
+      // tunnel the browsersync server through a random public url (activate if needed), first option for random subdomain, second option if you want to try a specific one -> only use one of them
+      // tunnel: true,
+      // attempt to use URL "http://werbelinie.localtunnel.me"
+      // tunnel: 'werbelinie',
     }
   }
 }
