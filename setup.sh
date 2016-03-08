66686bffb5e783d255949ca11c128103d7df66a8
Raphael Hüni <hueni@werbelinie.ch>
2016-03-08 15:32:38 +0100

    feat: auto setup after npm install
    Asks for ACF Pro key during installation process. If none is entered the
    dependency is changed to the basic ACF version from the WordPress
    Plugin Repository, so the install is not broken because of a missing key.
    The key is only written to the file if it has not previously existed.

---
+++ setup.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+if [ -a secrets.json ]
+then
+  grunt setup
+else
+  cp secrets-template.json secrets.json
+  read -p 'Paste your ACF Pro Key here: ' answer
+  if [[ -z "$answer" ]];
+  then
+    echo 'As you do not have an ACF Pro Key we will change the dependency to use the basic ACF version found in the WordPress plugin registry.'
+    sed -i '.bak' 's,"advanced-custom-fields/advanced-custom-fields-pro":,"wpackagist-plugin/advanced-custom-fields":,g' composer.json
+    rm composer.json.bak
+  else
+    echo 'Writing the ACF Key to your secrets.json. Please do not put this key in a public repo.'
+    sed -i ".bak" "s/@@acfProKey/${answer}/g" secrets.json
+    rm secrets.json.bak
+  fi
+  grunt setup
+fi
