a8b4c839ef99a9eae9c04046ae71d51cd7ef916e
Raphael Hüni <hueni@werbelinie.ch>
2016-03-08 07:02:28 +0100

    fix(grunt): remove some not needed configuration for changelog

--- grunt/conventionalChangelog.js
+++ grunt/conventionalChangelog.js
@@ -5,18 +5,6 @@ module.exports = {
       preset: 'angular',
       // activate the following line to regenarate your changelog (in case some commits went missing from it), make sure to delete the contents of CHANGELOG.md before)
       // releaseCount: 0
-    },
-    context: {
-      // context goes here
-    },
-    gitRawCommitsOpts: {
-      // git-raw-commits options go here
-    },
-    parserOpts: {
-      // conventional-commits-parser options go here
-    },
-    writerOpts: {
-      // conventional-changelog-writer options go here
     }
   },
   release: {
