0f1406184ba8b46463ec2cdbd199904f76b5001d
Raphael Hüni <hueni@werbelinie.ch>
2016-03-08 08:12:49 +0100

    feat(grunt): add some plugin customization for svgmin

--- grunt/svgmin.js
+++ grunt/svgmin.js
@@ -1,10 +1,19 @@
 module.exports = {
+  options: {
+    plugins: [
+      {
+        removeViewBox: false
+      }, {
+        sortAttrs: true
+      }
+    ]
+  },
   dist: {
-        files: [{
-            expand: true,
-            cwd: '<%= config.srcFolders.icons %>',
-            src: ['*.svg'],
-            dest: '<%= config.srcFolders.iconsMin %>'
-        }]
+    files: [{
+        expand: true,
+        cwd: '<%= config.srcFolders.icons %>',
+        src: ['*.svg'],
+        dest: '<%= config.srcFolders.iconsMin %>'
+    }]
     }
 }
