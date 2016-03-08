507cc82f84c6c52db823e0debb5c503d472212bd
Raphael Hüni <hueni@werbelinie.ch>
2016-03-08 08:05:58 +0100

    feat(grunt): update shell tasks to use shorthand
    Also remove one deprecated task no longer needed.
    
    BREAKING CHANGE: shell:getWPold is no longer available.

--- grunt/shell.js
+++ grunt/shell.js
@@ -1,29 +1,10 @@
 module.exports = {
-  bower: {
-    command: 'bower install'
-  },
-  bowerUpdate: {
-    command: 'bower update'
-  },
-  addChangelog: {
-    command: 'git add CHANGELOG.md && git commit --amend --no-edit && git tag v<%= pkg.version %> -m "Version <%= pkg.version %>" && git push && git push --tags'
-  },
-  getWPold: {
-    command: 'wget https://wordpress.org/latest.tar.gz && tar zxvf latest.tar.gz && mv wordpress <%= config.rootFolder %> && rm latest.tar.gz'
-  },
-  getWP: {
-    command: 'sh getWP.sh <%= config.rootFolder %>'
-  },
-  getKeys: {
-    command: 'sh getKeys.sh keys.php'
-  },
-  installPlugins: {
-    command: 'composer self-update && composer install --no-dev'
-  },
-  updatePlugins: {
-    command: 'composer self-update && composer update --no-dev'
-  },
-  updateUrl: {
-    command: 'sed -i "" "s=<%= config.devUrl %>=http://${PWD##*/}.dev=g" Gruntconfig.yml'
-  }
+  bower: 'bower install',
+  bowerUpdate: 'bower update',
+  addChangelog: 'git add CHANGELOG.md && git commit --amend --no-edit && git tag v<%= pkg.version %> -m "Version <%= pkg.version %>" && git push && git push --tags',
+  getWP: 'sh getWP.sh <%= config.rootFolder %>',
+  getKeys: 'sh getKeys.sh keys.php',
+  installPlugins: 'composer self-update && composer install --no-dev',
+  updatePlugins: 'composer self-update && composer update --no-dev',
+  updateUrl: 'sed -i "" "s=<%= config.devUrl %>=http://${PWD##*/}.dev=g" Gruntconfig.yml'
 }
