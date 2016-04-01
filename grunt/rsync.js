e574de9e3417703989ea01a76a6971396ea963d0
Raphael Hüni <hueni@werbelinie.ch>
2016-04-01 15:16:36 +0200

    fix(grunt): include the wp-content/uploads folder for the first push
    When pushing for the very first time the uploads folder should usually
    be included as it might contain images or files that should end up on
    the live site and have been integrated during the development process
    locally. Therefore we remove it from the array of excluded file paths.

--- grunt/rsync.js
+++ grunt/rsync.js
@@ -5,7 +5,7 @@ module.exports = {
     recursive: true,
     // use commented out line after first push to staging and live server unless you change something in your configuration file since some plugins (mainly wp super cache) write directly to the wp-config file.
     // before pushing wp-config.php changes anew make sure to copy down all online changes into your src/config folder
-    exclude: ['.git', '.hg', '.svn', '.DS_Store', '._*', 'Thumbs.db', '/wp-content/uploads/**', '/wp-content/backupwordpress*', '.map'],
+    exclude: ['.git', '.hg', '.svn', '.DS_Store', '._*', 'Thumbs.db', '/wp-content/backupwordpress*', '.map'],
     // exclude: ['.git', '.hg', '.svn', '.DS_Store', '._*', 'Thumbs.db', '/wp-content/uploads/**', '/wp-config.php', '/wp-content/backupwordpress*', '.map'],
     compareMode: 'checksum'
   },
