6f33927bebb87620cdbea2d302d5e17ef5882160
Raphael Hüni <hueni@werbelinie.ch>
2016-04-08 08:35:29 +0200

    fix(php): replace wrong textdomains

--- src/php/template-parts/content-none.php
+++ src/php/template-parts/content-none.php
@@ -18,14 +18,14 @@
 
 if ( is_home() && current_user_can( 'publish_posts' ) ) : ?>
 
-  <p><?php printf( __( 'Ready to publish your first post? <a href="%1$s">Get started here</a>.', 'twentysixteen' ), esc_url( admin_url( 'post-new.php' ) ) ); ?></p>
+  <p><?php printf( __( 'Ready to publish your first post? <a href="%1$s">Get started here</a>.', 'rafhun' ), esc_url( admin_url( 'post-new.php' ) ) ); ?></p>
 <?php elseif ( is_search() ) : ?>
-  <p><?php _e( 'Sorry, but nothing matched your search terms. Please try again with some different keywords.', 'twentysixteen' ); ?></p>
+  <p><?php _e( 'Sorry, but nothing matched your search terms. Please try again with some different keywords.', 'rafhun' ); ?></p>
   <?php get_search_form(); ?>
 
 <?php else : ?>
 
-  <p><?php _e( 'It seems we can&rsquo;t find what you&rsquo;re looking for. Perhaps searching can help.', 'twentysixteen' ); ?></p>
+  <p><?php _e( 'It seems we can&rsquo;t find what you&rsquo;re looking for. Perhaps searching can help.', 'rafhun' ); ?></p>
   <?php get_search_form(); ?>
 
 <?php endif; ?>
