c260883424046f45c1c7c1b376cbcf2969282c7f
Raphael Hüni <hueni@werbelinie.ch>
2016-04-11 10:37:25 +0200

    fix(setup): cleaner setup process for the acf key

--- setup.sh
+++ setup.sh
@@ -9,12 +9,10 @@ else
   if [[ -z "$answer" ]];
   then
     echo 'As you do not have an ACF Pro Key we will change the dependency to use the basic ACF version found in the WordPress plugin registry.'
-    sed -i '.bak' 's,"advanced-custom-fields/advanced-custom-fields-pro":,"wpackagist-plugin/advanced-custom-fields":,g' composer.json
-    rm composer.json.bak
+    sed -i '' 's,"advanced-custom-fields/advanced-custom-fields-pro":,"wpackagist-plugin/advanced-custom-fields":,g' composer.json
   else
     echo 'Writing the ACF Key to your secrets.json. Please do not put this key in a public repo.'
-    sed -i ".bak" "s/@@acfProKey/${answer}/g" secrets.json
-    rm secrets.json.bak
+    sed -i "" "s/@@acfProKey/${answer}/g" secrets.json
   fi
   grunt setupGruntconfig
   grunt setup
