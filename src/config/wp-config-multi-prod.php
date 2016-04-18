1ca151cba74d7e2c4e90d91242ad5da1259319b5
Raphael Hüni <hueni@werbelinie.ch>
2016-04-18 11:12:56 +0200

    fix(php): cleanup wp-config templates

--- src/config/wp-config-multi-prod.php
+++ src/config/wp-config-multi-prod.php
@@ -1,35 +1,14 @@
 <?php
 /**
  * In dieser Datei werden die Grundeinstellungen für WordPress vorgenommen.
- *
- * Zu diesen Einstellungen gehören: MySQL-Zugangsdaten, Tabellenpräfix,
- * Secret-Keys, Sprache und ABSPATH. Mehr Informationen zur wp-config.php gibt es
- * auf der {@link http://codex.wordpress.org/Editing_wp-config.php wp-config.php editieren}
- * Seite im Codex. Die Informationen für die MySQL-Datenbank bekommst du von deinem Webhoster.
- *
- * Diese Datei wird von der wp-config.php-Erzeugungsroutine verwendet. Sie wird ausgeführt,
- * wenn noch keine wp-config.php (aber eine wp-config-sample.php) vorhanden ist,
- * und die Installationsroutine (/wp-admin/install.php) aufgerufen wird.
- * Man kann aber auch direkt in dieser Datei alle Eingaben vornehmen und sie von
- * wp-config-sample.php in wp-config.php umbenennen und die Installation starten.
- *
  * @package WordPress
  */
 
 /**  MySQL Einstellungen - diese Angaben bekommst du von deinem Webhoster. */
-/**  Ersetze database_name_here mit dem Namen der Datenbank, die du verwenden möchtest. */
 define('DB_NAME', '@@db_name');
-
-/** Ersetze username_here mit deinem MySQL-Datenbank-Benutzernamen */
 define('DB_USER', '@@db_user');
-
-/** Ersetze password_here mit deinem MySQL-Passwort */
 define('DB_PASSWORD', '@@db_password');
-
-/** Ersetze localhost mit der MySQL-Serveradresse */
 define('DB_HOST', '@@db_host');
-
-/** Der Datenbankzeichensatz der beim Erstellen der Datenbanktabellen verwendet werden soll */
 define('DB_CHARSET', 'utf8');
 
 /** Der collate type sollte nicht geändert werden */
@@ -37,25 +16,14 @@ define('DB_COLLATE', '');
 
 /**#@+
  * Sicherheitsschlüssel
- *
- * Ändere jeden KEY in eine beliebige, möglichst einzigartige Phrase.
- * Auf der Seite {@link https://api.wordpress.org/secret-key/1.1/salt/ WordPress.org secret-key service}
- * kannst du dir alle KEYS generieren lassen.
- * Bitte trage für jeden KEY eine eigene Phrase ein. Du kannst die Schlüssel jederzeit wieder ändern,
- * alle angemeldeten Benutzer müssen sich danach erneut anmelden.
- *
  * @seit 2.6.0
  */
-
 @@include
 
 /**#@-*/
 
 /**
  * WordPress Datenbanktabellen-Präfix
- *
- *  Wenn du verschiedene Präfixe benutzt, kannst du innerhalb einer Datenbank
- *  verschiedene WordPress-Installationen betreiben. Nur Zahlen, Buchstaben und Unterstriche bitte!
  */
 $table_prefix  = '@@db_prefix';
 
