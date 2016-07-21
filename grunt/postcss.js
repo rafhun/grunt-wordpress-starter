1dbe27eafc426b3a0a0e5d4cc480e34edda689bb
Raphael Hüni <hueni@werbelinie.ch>
2016-07-21 13:54:01 +0200

    fix(grunt): keep ios saf 8 support after autoprefixer bump

--- grunt/postcss.js
+++ grunt/postcss.js
@@ -6,7 +6,7 @@ module.exports = {
     },
 
     processors: [
-      require( 'autoprefixer' )( { browsers: 'last 2 versions' } )
+      require( 'autoprefixer' )( { browsers: 'last 3 versions' } )
     ]
   },
   main: {
